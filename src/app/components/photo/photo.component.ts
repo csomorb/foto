4b82dbd39b66c98fa104bbe9a860aaa24f01fe2a
csomorb <csomorbarnabas@yahoo.com>
2020-11-11 17:55:17 +0100

    photo navigation

--- src/app/components/photo/photo.component.ts
+++ src/app/components/photo/photo.component.ts
@@ -3,6 +3,7 @@ import { environment } from 'src/environments/environment';
 import { PhotoModel } from 'src/app/models/photo.model';
 import { AlbumService } from 'src/app/services/album.service';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 export enum KEY_CODE {
   RIGHT_ARROW = 39,
@@ -19,14 +20,21 @@ export class PhotoComponent implements OnInit {
 
   photoBaseUrl: string = environment.apiUrl;
 
-  constructor(public albumService: AlbumService, private router: Router, private route: ActivatedRoute) { }
+  constructor(public albumService: AlbumService, private router: Router,
+    private location: Location, private route: ActivatedRoute) { }
 
   moveNextPhoto(){
+    let previousPhotoId = this.albumService.currentPhoto.idPhoto;
     this.albumService.moveNextPhoto();
+    let cururl = this.location.path().replace( "photos/"+previousPhotoId, "photos/" + this.albumService.currentPhoto.idPhoto );
+    this.location.go(cururl);
   }
 
   movePrevPhoto(){
+    let previousPhotoId = this.albumService.currentPhoto.idPhoto;
     this.albumService.movePrevPhoto();
+    let cururl = this.location.path().replace( "photos/"+previousPhotoId, "photos/" + this.albumService.currentPhoto.idPhoto );
+    this.location.go(cururl);
   }
 
   @HostListener('window:keyup', ['$event'])
